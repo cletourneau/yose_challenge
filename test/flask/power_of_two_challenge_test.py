d9f995b246fa81e2acfe101faecc9dedcabb423f
Carl Letourneau <cletourneau@gmail.com>
2013-11-16 00:05:17 -0500

    Returns an error when number is too big for the power of two challenge

--- test/flask/power_of_two_challenge_test.py
+++ test/flask/power_of_two_challenge_test.py
@@ -1,4 +1,5 @@
 from httplib import BAD_REQUEST
+import json
 import unittest
 from hamcrest import is_, assert_that
 from nose.tools import istest
@@ -10,8 +11,33 @@ class PowerOfTwoChallengeTest(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
-        self.response = self.app.get('/primeFactors?number=hello')
 
     @istest
-    def responds_bad_request(self):
-        assert_that(self.response.status_code, is_(BAD_REQUEST))
+    def responds_bad_request_when_not_a_number(self):
+        response = self.app.get('/primeFactors?number=')
+        assert_that(response.status_code, is_(BAD_REQUEST))
+
+    @istest
+    def repeats_bad_number_when_not_a_number(self):
+        response = self.app.get('/primeFactors?number=yoyo')
+        assert_that(json.loads(response.data)['number'], is_('yoyo'))
+
+    @istest
+    def says_its_not_a_number_when_not(self):
+        response = self.app.get('/primeFactors?number=yaya')
+        assert_that(json.loads(response.data)['error'], is_('not a number'))
+
+    @istest
+    def responds_bad_request_when_too_big(self):
+        response = self.app.get('/primeFactors?number=1000001')
+        assert_that(response.status_code, is_(BAD_REQUEST))
+
+    @istest
+    def repeats_bad_number_when_too_big(self):
+        response = self.app.get('/primeFactors?number=1000001')
+        assert_that(json.loads(response.data)['number'], is_('1000001'))
+
+    @istest
+    def describes_error_when_too_big(self):
+        response = self.app.get('/primeFactors?number=1000001')
+        assert_that(json.loads(response.data)['error'], is_('too big number (>1e6)'))
--- yose/power_of_two_challenge.py
+++ yose/power_of_two_challenge.py
@@ -3,18 +3,29 @@ from flask import make_response, jsonify, request
 from yose import APP as app
 from yose.prime_factors import prime_factors_of
 
+BIGGEST_SUPPORTED_NUMBER = 1e6
+
 
 @app.route('/primeFactors', methods=['GET'])
 def power_of_two():
     number = request.args.get('number')
 
-    if not a_number(number):
+    if not is_a_number(number):
         return not_a_number_error(number)
+    elif number_is_too_big(int(number)):
+        return number_too_big_error(number)
     else:
         return prime_factors_response(int(number))
 
 
-def a_number(s):
+def prime_factors_response(number):
+    return make_response(jsonify({
+        'number': number,
+        'decomposition': prime_factors_of(number)
+    }))
+
+
+def is_a_number(s):
     try:
         int(s)
         return True
@@ -29,8 +40,14 @@ def not_a_number_error(number):
     }), BAD_REQUEST)
 
 
-def prime_factors_response(number):
+def number_is_too_big(number):
+    return number > BIGGEST_SUPPORTED_NUMBER
+
+
+def number_too_big_error(number):
     return make_response(jsonify({
         'number': number,
-        'decomposition': prime_factors_of(number)
-    }))
+        'error': 'too big number (>1e6)'
+    }), BAD_REQUEST)
+
+
