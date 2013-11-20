0928a2d405a445330b0a32c013b9bf4d8bcd004f
Carl Letourneau <cletourneau@gmail.com>
2013-11-19 20:40:43 -0500

    Form now shows a result (hardcoded)

--- features/challenges/world2/level2_6_test.py
+++ features/challenges/world2/level2_6_test.py
@@ -1,6 +1,6 @@
 import unittest
 from features.challenges.world1 import disable_annoying_selenium_logs, phantomjs_path
-from hamcrest import assert_that
+from hamcrest import assert_that, is_
 from nose.plugins.attrib import attr
 from nose.tools import istest
 from splinter import Browser
@@ -23,3 +23,13 @@ class PrimeFactorsFormTest(unittest.TestCase):
             assert_that(browser.is_element_present_by_id('invitation'))
             assert_that(browser.is_element_present_by_css('form input#number'))
             assert_that(browser.is_element_present_by_css('form button#go'))
+
+    @istest
+    def displays_the_decomposition_of_the_input_number(self):
+        with Browser(driver_name='phantomjs', executable_path=phantomjs_path()) as browser:
+            browser.visit(self.server_url + '/primeFactors/ui')
+
+            browser.find_by_css('form input#number').first.fill('3240')
+            browser.find_by_css('form button#go').first.click()
+
+            assert_that(browser.find_by_id('result').first.value, is_('3240 = 2 x 2 x 2 x 3 x 3 x 3 x 3 x 5'))
