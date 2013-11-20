1e2c19178e9a9c88d90a123a3dbe3a86d9948590
Carl Letourneau <cletourneau@gmail.com>
2013-11-19 21:11:32 -0500

    Prime factors UI now posts on itself and show the results

--- yose/endpoints/prime_factors_ui.py
+++ yose/endpoints/prime_factors_ui.py
@@ -1,6 +1,13 @@
-from flask import render_template
+from flask import render_template, request
 from yose import APP as app
+from yose.prime_factors import prime_factors_of
 
-@app.route('/primeFactors/ui', methods=['GET'])
+
+@app.route('/primeFactors/ui', methods=['GET', 'POST'])
 def display_form():
+    if request.method == 'POST':
+        number = request.form['number']
+        prime_factors = prime_factors_of(int(number))
+        result = "{number} = {factors}".format(number=number, factors=' x '.join([str(p) for p in prime_factors]))
+        return render_template('primeFactorsForm.jade', result=result)
     return render_template('primeFactorsForm.jade')
